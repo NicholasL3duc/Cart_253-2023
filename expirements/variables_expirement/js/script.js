9539abde5e48036ec45256d8f2f88049282c2b5f
NicholasL3duc <nicholas.leduc1@gmail.com>
2023-09-18 19:39:30 -0400

    created the bear with the movable hat
    need to change the colour of the bears brows, and hopefully the background

--- expirements/variables_expirement/js/script.js
+++ expirements/variables_expirement/js/script.js
@@ -13,6 +13,8 @@
 */
 function preload() {
 
+
+
 }
 
 
@@ -21,6 +23,9 @@ function preload() {
 */
 function setup() {
 
+createCanvas(500, 500);
+
+
 }
 
 
@@ -28,5 +33,89 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+   background (255,255,255);
+
+    
+
+noStroke();
+// this is the body
+fill(199,21,133);
+ellipse(320,400,220,200);
+
+fill(255,228,196);
+ellipse(320,400,190,170);
+
+// this is the right Ear
+fill(199,21,133);
+ellipse(360,220,80,70);
+
+fill(255,228,196);
+ellipse(360,220,60,50);
+
+
+// this is the left Ear
+fill(199,21,133);
+ellipse(280,220,80,70);
+
+fill(255,228,196);
+ellipse(280,220,60,50);
+
+
+
+// this is the Head
+fill(199,21,133);
+ellipse(320,270,150,120);
+
+fill(255,228,196);
+ellipse(320,280,110,85);
+
+//eyes
+//left eye
+fill(255,250,240);
+ellipse(300,230,22,20);
+
+
+
+//right eye
+fill(255,250,240);
+ellipse(340,230,22,20);
+
+
+//mouth
+fill(199,21,133);
+ellipse(320,275,60,40);
+    
+ // hat attempt
+fill(105,105,105)
+ rect(250, mouseY, 140,20);
+
+ fill(0)
+rect(290,mouseY, 60,-70);
+
+
+// angry brows
+rect.fill = map(0,mouseY,width,0,15);
+fill(rect.fill);
+fill(mouseY)
+   rect(275,200,40,15);
+
+fill(mouseY)
+   rect(320,200,40,15);
+
+
+   
+
+
+    
+
+    
+
+
+
+   
+
+
+
+
 
 }
