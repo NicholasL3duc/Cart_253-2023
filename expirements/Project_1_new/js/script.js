fdfde63c7e456a468e46f8d1e265c23ffde14a2f
NicholasL3duc <nicholas.leduc1@gmail.com>
2023-10-13 18:21:34 -0400

    got most of the maze done
    the water walls are done, adding the puddle run next then the moving predators
    
    need to workout the key image and the exit

--- expirements/Project_1_new/js/script.js
+++ expirements/Project_1_new/js/script.js
@@ -18,6 +18,8 @@ function preload() {
 let Krab = {
     x: 50,
     y: 250,
+    w:20,
+    h:20,
     headSizeK:70,
     eyes:{
         size: 15,
@@ -51,7 +53,7 @@ let waterMaze ={
             y:0,
             w:100,
             h:460,
-
+        },
         Wall2:{
             x:220,
             y:360,
@@ -65,10 +67,56 @@ let waterMaze ={
             w:260,
             h:110,
         },
+        Wall4:{
+            x:500,
+            y:0,
+            w:100,
+            h:200,
+        },
+        Wall5:{
+            x:590,
+            y:360,
+            w:400,
+            h:100,
+        },
+        Wall6:{
+            x:590,
+            y:360,
+            w:110,
+            h:500,
+        },
+        Wall7:{
+            x:900,
+            y:360,
+            w:110,
+            h:300,
+        }
+
+    };
+let puddle = {
+
+    Jump1:{
+        x:10,
+        y: 10,
+    },
+    Jump2:{
 
     },
-   
+    Jump3:{
+
+    },
+    Jump4:{
+
+    },
+    Jump5:{
+
+    }
+
+
+
 };
+   
+
 let state = 'title';
 
 /**
@@ -78,10 +126,6 @@ function setup() {
 
 // background info
     createCanvas (windowWidth,windowHeight);
-
-
-
-
 
 }
 
@@ -107,9 +151,7 @@ if (state === 'title'){
     else if (state === 'Loss') {
      Loss();
     }
- else if (state === 'test'){
-    test();
- }
+ 
 
 function title(){
     push();
@@ -137,10 +179,7 @@ function Loss(){
     text('Krab Soup For You :(', width/2,height/2);
     pop();
 }
-function test (){
-    fill(255, 255, 0);
-    ellipse(Krab.x,Krab.y,Krab.w,Krab.h)
-}
+
 
 function simulation(){
     checkOffscreen();
@@ -188,7 +227,7 @@ noStroke();
    ellipse(Krab.x,Krab.y,60,40,Krab.headSizeK);
 // legs
     fill (194, 99, 31);
-    rect(Krab.legs.x,Krab.legs.y,Krab.legs.size);
+    line(Krab.legs.x,Krab.legs.y,Krab.legs.size);
 // claws
 
 //eyes
@@ -206,22 +245,28 @@ noStroke();
   
     // water walls
      fill (11, 139, 230);
-    // wall1
-     rect(waterMaze.Wall1.x,waterMaze.Wall1.y,waterMaze.Wall1.w,waterMaze.Wall1.h);
-    // Wall 2
-    //fill (11, 139, 230);
-    //rect(waterMaze.Wall2.x,waterMaze.Wall2.y,waterMaze.Wall2.w,waterMaze.Wall2.h);
-    // // Wall 3
-    // rect(waterMaze.Wall3.x,waterMaze.Wall3.y,waterMaze.Wall3.w,waterMaze.Wall3.h);
-    // // // Wall 4
-    // rect(waterMaze.Wall4.x,waterMaze.Wall4.y,waterMaze.Wall4.w,waterMaze.Wall4.h);
-    // // Wall 5
-    // rect(waterMaze.Wall5.x,waterMaze.Wall5.y,waterMaze.Wall5.w,waterMaze.Wall5.h);
-    // // Wall 6
-    // rect(waterMaze.Wall6.x,waterMaze.Wall6.y,waterMaze.Wall6.w,waterMaze.Wall6.h);
-    // // Wall 7
-    // rect(waterMaze.Wall7.x,waterMaze.Wall7.y,waterMaze.Wall7.w,waterMaze.Wall7.h);
+        // wall1
+        rect(waterMaze.Wall1.x,waterMaze.Wall1.y,waterMaze.Wall1.w,waterMaze.Wall1.h);
+        // Wall 2
+        fill (11, 139, 230);
+        rect(waterMaze.Wall2.x,waterMaze.Wall2.y,waterMaze.Wall2.w,waterMaze.Wall2.h);
+        // // Wall 3
+        rect(waterMaze.Wall3.x,waterMaze.Wall3.y,waterMaze.Wall3.w,waterMaze.Wall3.h);
+        // // // Wall 4
+        rect(waterMaze.Wall4.x,waterMaze.Wall4.y,waterMaze.Wall4.w,waterMaze.Wall4.h);
+        // // Wall 5
+        rect(waterMaze.Wall5.x,waterMaze.Wall5.y,waterMaze.Wall5.w,waterMaze.Wall5.h);
+        // // Wall 6
+        rect(waterMaze.Wall6.x,waterMaze.Wall6.y,waterMaze.Wall6.w,waterMaze.Wall6.h);
+        // // Wall 7
+        rect(waterMaze.Wall7.x,waterMaze.Wall7.y,waterMaze.Wall7.w,waterMaze.Wall7.h);
 
+    // Shark Run
+
+
+    // Puddle Run
+    fill(11, 139, 230)
+    ellipse(puddle.Jump1.x,puddle.Jump1.y)
   
 
 
@@ -238,17 +283,66 @@ function checkOffscreen(){
         }
     }
     function checkOverlap(){
-         //rect(430,0, 100, 110);
-     if (Krab.y < waterMaze.Wall1.x -waterMaze.Wall1.h || Krab.x > waterMaze.Wall1.w || Krab.y < waterMaze.Wall1.y ){
-        //if (Krab.x > waterMaze.Wall1.x - waterMaze.Wall1.w && Krab.x < waterMaze.Wall1.x + waterMaze.Wall1.w && Krab.y > waterMaze.Wall1.y - waterMaze.Wall1.h && Krab.y <  waterMaze.Wall1.y + waterMaze.Wall1.h){
-         state = 'loss';
+            //over lap check for wall 1
+            if (Krab.x + Krab.w > waterMaze.Wall1.x &&
+                Krab.x < waterMaze.Wall1.x + waterMaze.Wall1.w &&
+                Krab.y + Krab.h > waterMaze.Wall1.y &&
+                Krab.y < waterMaze.Wall1.y + waterMaze.Wall1.h){
+         state = 'Loss';
       }
+        //over lap check for wall 2
+        if (Krab.x + Krab.w > waterMaze.Wall2.x &&
+            Krab.x < waterMaze.Wall2.x + waterMaze.Wall2.w &&
+            Krab.y + Krab.h > waterMaze.Wall2.y &&
+            Krab.y < waterMaze.Wall2.y + waterMaze.Wall2.h){
+     state = 'Loss';
+  }
+        //over lap check for wall 3
+        if (Krab.x + Krab.w > waterMaze.Wall3.x &&
+            Krab.x < waterMaze.Wall3.x + waterMaze.Wall3.w &&
+            Krab.y + Krab.h > waterMaze.Wall3.y &&
+            Krab.y < waterMaze.Wall3.y + waterMaze.Wall3.h){
+     state = 'Loss';
+  }
+        //over lap check for wall 4
+        if (Krab.x + Krab.w > waterMaze.Wall4.x &&
+            Krab.x < waterMaze.Wall4.x + waterMaze.Wall4.w &&
+            Krab.y + Krab.h > waterMaze.Wall4.y &&
+            Krab.y < waterMaze.Wall4.y + waterMaze.Wall4.h){
+     state = 'Loss';
+  }
+        //over lap check for wall 5
+        if (Krab.x + Krab.w > waterMaze.Wall5.x &&
+            Krab.x < waterMaze.Wall5.x + waterMaze.Wall5.w &&
+            Krab.y + Krab.h > waterMaze.Wall5.y &&
+            Krab.y < waterMaze.Wall5.y + waterMaze.Wall5.h){
+     state = 'Loss';
+  }
+        //over lap check for wall 6
+        if (Krab.x + Krab.w > waterMaze.Wall6.x &&
+            Krab.x < waterMaze.Wall6.x + waterMaze.Wall6.w &&
+            Krab.y + Krab.h > waterMaze.Wall6.y &&
+            Krab.y < waterMaze.Wall6.y + waterMaze.Wall6.h){
+     state = 'Loss';
+  }
+        //over lap check for wall 7
+        if (Krab.x + Krab.w > waterMaze.Wall7.x &&
+            Krab.x < waterMaze.Wall7.x + waterMaze.Wall7.w &&
+            Krab.y + Krab.h > waterMaze.Wall7.y &&
+            Krab.y < waterMaze.Wall7.y + waterMaze.Wall7.h){
+     state = 'Loss';
+  }
+
+
+
+
+
+
         }
    
 
 
-//   || Krab.y > waterMaze.Wall1.h  || Krab.x > waterMaze.Wall1.w 
-;
+
     
 function movements(){
 
