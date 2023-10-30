2cf3f3d23079bfcdb7dfa41d793e26f3c222dbb6
NicholasL3duc <nicholas.leduc1@gmail.com>
2023-10-30 12:52:31 -0400

    created the Garden object
    followed both the video and the text to set up my garden object the way i wanted, started the array for the flowers and added color

--- expirements/The_Garden_Project/js/script.js
+++ expirements/The_Garden_Project/js/script.js
@@ -8,6 +8,25 @@
 
 "use strict";
 
+// this is the poluted garden
+let garden = {
+// the array for the decomposing flowers
+flowers: [],
+// the number of flowers at the start
+numflowers: 21,
+// the color of the polutted grass
+grassColor: {
+r: 48,
+g: 102,
+b: 56,
+
+
+}
+
+
+
+
+}
   // setup() creates the canvas and the flowers in the garden
   function setup() {
     createCanvas(600, 600);
