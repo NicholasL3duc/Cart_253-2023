f0c79617ee83cfd574c68206b1a85a748adf51bd
NicholasL3duc <nicholas.leduc1@gmail.com>
2023-11-10 16:53:45 -0500

    i lied... i know nothing
    some of the code makes sense to me but the setTimeout function doesnt, will look into it further

--- expirements/final stage of the final project/js/script.js
+++ expirements/final stage of the final project/js/script.js
@@ -1,32 +1,36 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
 "use strict";
 
-let notes = [ 60,62,64,65,67,69,71];
-let osc
-
+let notes = [60, 62, 64, 65, 67, 69, 71];
+let osc;
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-createCanvas(1400, 850)
-osc = new p5.triOcs();
-osc.start();
-osc.amp(0)
-
+  createCanvas(1400, 850);
+  osc = new p5.triOcs();
+  osc.start();
+  osc.amp(0);
 }
-
+function playNote(note, duration) {
+  osc.freq(miditofreq(note));
+  osc.fade(0.5, 0.2);
+  if (duration) {
+    setTimeout(function () {
+      osc.fade(0, 0.2);
+    }, duration - 50);
+  }
+}
 
 /**
  * Description of draw()
-*/
-function draw() {
-
-}
+ */
+function draw() {}
